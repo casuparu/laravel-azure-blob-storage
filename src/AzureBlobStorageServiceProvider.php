db5783850516fb6fbea67e6abb962190a20ebaf5
Caspar Mølholt <Kjellberg>
2019-03-19 09:38:44 +0100

    Added the possibility to provide a specific blob endpoint.

--- src/AzureBlobStorageServiceProvider.php
+++ src/AzureBlobStorageServiceProvider.php
@@ -29,22 +29,35 @@ class AzureBlobStorageServiceProvider extends ServiceProvider
     public function boot()
     {
         Storage::extend('azure', function ($app, $config) {
-            $endpoint = sprintf(
-                'DefaultEndpointsProtocol=https;AccountName=%s;AccountKey=%s',
-                $config['name'],
-                $config['key']
-            );
-
-            /*
-             * When sig= is found in the key, we assume that we must build a ConnectionString from the SAS principle.
-             */
-            if (strpos($config['key'],'sig=') !== false)
-            {
+            if ($config['local'] == true) {
+                $endpoint = 'UseDevelopmentStorage=true';
+            } else {
                 $endpoint = sprintf(
-                    'BlobEndpoint=https://%s.blob.core.windows.net;SharedAccessSignature=%s',
+                    'DefaultEndpointsProtocol=https;AccountName=%s;AccountKey=%s;',
                     $config['name'],
                     $config['key']
                 );
+
+                /*
+                 * If an endpoint url is specified in the environment, use that
+                 */
+                if ($config['url'] != '') {
+                    $endpoint .= 'BlobEndpoint=' . (($config['https'] == true) ? 'https' : 'http') . '://' 
+                        . $config['url'] . ':' . $config['port'] . '/' . $config['name'] . ';';
+                }
+                
+                /*
+                 * When sig= is found in the key, we assume that we must build a ConnectionString from the SAS principle.
+                 */
+                if (strpos($config['key'],'sig=') !== false)
+                {
+                    $endpoint = sprintf(
+                        'BlobEndpoint=https://%s.blob.core.windows.net;SharedAccessSignature=%s',
+                        $config['name'],
+                        $config['key']
+                    );
+                }
+
             }
 
             $client = BlobRestProxy::createBlobService($endpoint);
